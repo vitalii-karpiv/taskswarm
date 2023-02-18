00ab8c3b5d44051bacaabcc133c1e369e6311761
Vitalii Karpiv <vitalii.karpiv@plus4u.net>
2023-02-18 23:48:09 +0200

    Initial commit

---
+++ src/main/java/com/karpiv/taskswarm/TaskSwarmApplication.java
@@ -0,0 +1,13 @@
+package com.karpiv.taskswarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSwarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSwarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karpiv/taskswarm/TaskSwarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karpiv.taskswarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSwarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
